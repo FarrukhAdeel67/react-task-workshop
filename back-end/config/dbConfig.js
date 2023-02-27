7ddf4dce1ca3b2b2040a628725e69ba08c50e95d
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2023-02-27 22:43:41 +0500

    updated

--- back-end/config/dbConfig.js
+++ back-end/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://farrukhadeel:pps993icp956@cluster0.ysuiv.mongodb.net/workshop")
+mongoose.connect("mongodb+srv://farrukhadeel:<'password'>@cluster0.ysuiv.mongodb.net/workshop")
     .then(result => {
         console.log("Database connected Successfully!"); // this is for connection with database
     }).catch(err => {
